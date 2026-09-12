06f690948565fa6430329a443ff284974147d052
Shagor Robidas <roysagor88@gmail.com>
2026-09-12 16:22:16 +0600

    style: format string quotes in core urls

--- core/urls.py
+++ core/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pdf_editor.api.urls')),
+    path("", include('pdf_editor.api.urls')),
 
 ]
